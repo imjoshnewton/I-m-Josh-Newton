0f75a3c2c99510619a98b57ff01b2925a7f7b59f
newtonjd1 <josh.newton@me.com>
2017-03-15 12:13:01 -0400

    1.0.1
    Some minor changes to further setup my Gulp config and to include the
    build folder in order to allow cloning to my web server from my local
    machine.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# I-m-Josh-Newton
+# I'm Josh Newton
+
+This is the code for my personal profile / BIO website.
--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,40 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
 
 gulp.task('sass', function(){
-  return gulp.src('source/css/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // Using gulp-sass
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('build/css'))
+    return gulp.src('source/css/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // Using gulp-sass
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('source/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('build'))
     .pipe(browserSync.reload({
         stream: true
     }))
 });
 
 gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'build'
-    },
-  })
+    browserSync.init({
+        server: {
+          baseDir: 'build'
+        },
+    })
 })
 
-gulp.task('default', ['browserSync', 'sass'], function() {
-  gulp.watch('source/css/*.scss', ['sass']);
-  gulp.watch('source/*.html', browserSync.reload); 
-  gulp.watch('source/js/**/*.js', browserSync.reload); 
+gulp.task('default', ['browserSync', 'sass', 'useref'], function() {
+    gulp.watch('source/css/*.scss', ['sass']);
+    gulp.watch('source/*.html', ['useref']);
+    gulp.watch('source/js/**/*.js', browserSync.reload);
 });
-
