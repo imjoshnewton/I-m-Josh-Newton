6c36df821c03ef85892b897c87e7e41f93534b03
newtonjd1 <josh.newton@me.com>
2017-03-20 22:43:05 -0400

    Added SourceMaps to Gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
-require('es6-promise').polyfill()
+//require('es6-promise').polyfill();
 
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var autoprefixer = require('gulp-autoprefixer')
-var browserSync = require('browser-sync').create()
-var useref = require('gulp-useref')
-var uglify = require('gulp-uglify')
-var gulpIf = require('gulp-if')
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
 var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
   return gulp.src('source/css/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({
-          outputStyle: 'nested'
+          outputStyle: 'compressed'
         })).on('error', gutil.log)
         .pipe(autoprefixer()).on('error', gutil.log)
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/css')).on('error', gutil.log)
         .pipe(browserSync.reload({
           stream: true
@@ -24,7 +27,9 @@ gulp.task('sass', function () {
 gulp.task('useref', function () {
   return gulp.src('source/*.html')
     .pipe(useref())
+    .pipe(sourcemaps.init())
     .pipe(gulpIf('*.js', uglify())).on('error', gutil.log)
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('build'))
     .pipe(browserSync.reload({
       stream: true
